c8751f5e47e5e9c5c18018f6254f9d4579c28955
Sanny26 <santhoshini.g14@iiits.in>
2018-10-06 12:51:35 +0530

    Update templates

--- htrsite/home/urls.py
+++ htrsite/home/urls.py
@@ -6,4 +6,6 @@ from . import views
 # use re_path for using it along with regular expressions
 urlpatterns = [
 	path('', views.main, name='home'),
+	path('progess', views.progess, name='progess'),
+	path('faq', views.faq, name='faq')
 	]
--- htrsite/home/views.py
+++ htrsite/home/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render
 
+import os
+import json
+
 # Create your views here.
 def main(request):
 	template_name = "home/index.html"
-	return render(request, template_name, {'test': 'test'})
+	return render(request, template_name, {})
+
+def progess(request):
+	template_name = "home/progess.html"
+	filename = "files/progress.txt"
+
+	progess = []
+	context = {}
+	with open(filename, 'r') as file:
+		for line in file:
+			line = line.split(',')
+			line = [each.strip() for each in line]
+			progess.append(line)
+
+	context['scripts'] = progess
+
+	return render(request, template_name, context) 
+
+def faq(request):
+	template_name = "home/faq.html"
+	filename = "files/faq.json"
+
+	context = {}
+	with open(filename, 'r') as file:
+		data = json.load(file)	 
+		
+	context['data'] = data
+	return render(request, template_name, context)
+
